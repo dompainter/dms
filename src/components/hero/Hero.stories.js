60d3c231da47de9d325dbd41a2b15b73a0658f2e
dompainter <dominickpainter@gmail.com>
2019-12-03 21:51:35 +1100

    Hero updates

--- src/components/hero/Hero.stories.js
+++ src/components/hero/Hero.stories.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, text } from '@storybook/addon-knobs'
+import CenteredWrapper from '../../storybook/components/centered-wrapper'
 import Hero from '.'
+
+const Container = styled.div`
+  width: 80%;
+  height: 300px;
+`
 
 const stories = storiesOf('Hero', module)
 stories.addDecorator(withKnobs)
 
 stories
   .add('Default', () => (
-    <Hero>
-      {text('Content', 'Hello World')}
-    </Hero>
+    <CenteredWrapper>
+      <Container>
+        <Hero
+          imageUrl="https://picsum.photos/1280/720"
+          heading={text('Heading', 'Hello World')}
+          subHeading={text('SubHeading', 'Latest News')}
+        />
+      </Container>
+    </CenteredWrapper>
   ))
--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,16 +1,54 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import styled from 'styled-components'
+import getFont from '../../utils/font'
+import { white } from '../../utils/pallette'
 
-const Hero = ({ name }) => {
+const BackgroundImage = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  color: ${white};
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+  background-size: cover;
+  position: relative;
+`
+
+const TextContainer = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  bottom: 15%;
+  left: 5%;
+`
+
+const Heading = styled.span`
+  font: ${getFont({ size: 32, weight: 600, lineHeight: '1.4em' })};
+`
+
+const SubHeading = styled.span`
+  font: ${getFont({ size: 20 })};
+`
+
+const Hero = ({ imageUrl, heading, subHeading }) => {
   return (
-    <div>
-      Hero
-    </div>
+    <BackgroundImage imageUrl={imageUrl}>
+      <TextContainer>
+        <Heading>
+          {heading}
+        </Heading>
+        <SubHeading>
+          {subHeading}
+        </SubHeading>
+      </TextContainer>
+    </BackgroundImage>
   )
 }
 
 Hero.propTypes = {
-  name: propTypes.string
+  imageUrl: propTypes.string,
+  heading: propTypes.string,
+  subHeading: propTypes.string
 }
 
 export default Hero
