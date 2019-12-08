573778953644491c5cf6217238339cb67c8a135b
dompainter <dominickpainter@gmail.com>
2019-12-08 21:49:06 +1100

    card component

--- src/components/card/Card.stories.js
+++ src/components/card/Card.stories.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, text } from '@storybook/addon-knobs'
+import CenteredWrapper from '../../storybook/components/centered-wrapper'
 import Card from '.'
 
 const stories = storiesOf('Card', module)
 stories.addDecorator(withKnobs)
 
 stories
-    .add('Default', () => (
-        <Card>
-            {text('Content', 'Hello World')}
-        </Card>
-    ))
+  .add('Default', () => (
+    <CenteredWrapper>
+      <Card
+        heading={text('Heading', 'Heading')}
+        subHeading={text('SubHeading', 'SubHeading')}
+        sell={text('Sell', 'Hello World')}
+        imageUrl="https://picsum.photos/720/720"
+        onClick={action('onClick')}
+      />
+    </CenteredWrapper>
+  ))
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,16 +1,65 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import styled from 'styled-components'
+import getFont from '../../utils/font'
+import { white } from '../../utils/pallette'
 
-const Card = ({ name }) => {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const ImageBox = styled.div`
+  display: flex;
+  height: 250px;
+  width: 250px;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+  background-size: cover;
+  transition: all .2s ease-in-out;
+
+  :hover {
+    transform: scale(1.05);
+  }
+`
+
+const TextContainer = styled.div`
+  width: 250px;
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+`
+
+const Heading = styled.span`
+  font: ${getFont({ size: 32, weight: 600, lineHeight: '1.4em' })};
+`
+
+const SubHeading = styled.span`
+  font: ${getFont({ size: 20, lineHeight: '1.2em' })};
+`
+
+const Sell = styled.span`
+  font: ${getFont()};
+  padding-top: 10px;
+`
+
+const Card = ({ imageUrl, heading, subHeading, sell }) => {
   return (
-    <div>
-      Card
-    </div>
+    <Container>
+      <ImageBox imageUrl={imageUrl}/>
+      <TextContainer>
+        <Heading>{heading}</Heading>
+        <SubHeading>{subHeading}</SubHeading>
+        <Sell>{sell}</Sell>
+      </TextContainer>
+    </Container>
   )
 }
 
 Card.propTypes = {
-  name: propTypes.string
+  imageUrl: propTypes.string,
+  heading: propTypes.string,
+  subHeading: propTypes.string,
+  sell: propTypes.string
 }
 
 export default Card
