13114ad1e5777c8dc18a3eb70fb68c66494116cd
dompainter <dominickpainter@gmail.com>
2020-01-08 21:36:33 +1100

    Add color grid

--- src/components/colours/index.jsx
+++ src/components/colours/index.jsx
@@ -1,16 +1,66 @@
 import React from 'react'
-import propTypes from 'prop-types'
+import styled from 'styled-components'
+import {
+  black,
+  blue,
+  green,
+  midnight,
+  porcelain,
+  red,
+  white
+} from '../../utils/pallette'
+import getFont from '../../utils/font'
 
-const Colours = ({ name }) => {
+const Block = styled.div`
+  width: 150px;
+  height: 150px;
+  padding: 15px;
+`
+
+const Circle = styled.div`
+  width: 100%;
+  height: 100%;
+  background: ${({ colour }) => colour};
+  color: ${({ fontColour }) => fontColour};
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const ColourGrid = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 50px;
+  flex-wrap: wrap;
+`
+
+const Text = styled.span`
+  font: ${getFont({ size: 16 })};
+`
+
+const Colours = () => {
+  const colours = [
+    { name: 'Black', value: black, fontColour: white },
+    { name: 'Blue', value: blue, fontColour: white },
+    { name: 'Green', value: green, fontColour: white },
+    { name: 'Midnight', value: midnight, fontColour: white },
+    { name: 'Porcelain', value: porcelain, fontColour: midnight },
+    { name: 'Red', value: red, fontColour: white },
+    { name: 'White', value: white, fontColour: midnight }
+  ]
+
   return (
-    <div>
-      Colours
-    </div>
+    <ColourGrid>
+      {colours.map(({ name, value, fontColour }) => (
+        <Block key={name}>
+          <Circle colour={value} fontColour={fontColour}>
+            <Text>{name}</Text>
+          </Circle>
+        </Block>
+      ))}
+    </ColourGrid>
   )
-}
-
-Colours.propTypes = {
-  name: propTypes.string
 }
 
 export default Colours
